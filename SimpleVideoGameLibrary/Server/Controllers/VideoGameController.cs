63a5d8764d2fd5c0df1b2face1abf1d5c3524f9b
Isiah Jones <jones.isiah.blhs@gmail.com>
2023-10-29 12:09:07 -0400

    Add project files.

---
+++ SimpleVideoGameLibrary/Server/Controllers/VideoGameController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleVideoGameLibrary.Shared;
+
+namespace SimpleVideoGameLibrary.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        public async Task<ActionResult<List<VideoGame>>> GetAllVideoGames()
+        {
+            var list = new List<VideoGame> {
+                new VideoGame { Id = 1, Title = "Super Mario Bros.", Publisher = "Nintendo", ReleaseYear = 1985 },
+                new VideoGame { Id = 2, Title = "The Legend of Zelda", Publisher = "Nintendo", ReleaseYear = 1986 },
+            };
+
+            return Ok(list);
+        }
+    }
+}
---
+++ SimpleVideoGameLibrary/Shared/VideoGame.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleVideoGameLibrary.Shared
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string Publisher { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+    }
+}
